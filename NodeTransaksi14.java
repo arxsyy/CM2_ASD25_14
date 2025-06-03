7454b5ef07357692ae9e78adde09bf7f8aa50a62
arxsyy <marsyfernanda@gmail.com>
2025-06-03 13:50:55 +0700

    addClass NodeTransaksi

---
+++ NodeTransaksi14.java
@@ -0,0 +1,8 @@
+public class NodeTransaksi14 {
+    TransaksiPengisian14 data;
+    NodeTransaksi14 next;
+
+    public NodeTransaksi14(TransaksiPengisian14 data) {
+        this.data = data;
+    }
+}
