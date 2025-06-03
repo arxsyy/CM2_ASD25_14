1e865bfb878cfbd638877bb7931c1cf0022a9b04
arxsyy <marsyfernanda@gmail.com>
2025-06-03 13:50:19 +0700

    addClass TransaksiPengisian

---
+++ TransaksiPengisian14.java
@@ -0,0 +1,13 @@
+public class TransaksiPengisian14 {
+    Kendaraan14 kendaraan;
+    BBM14 bbm;
+    double liter;
+    double totalBayar;
+
+    public TransaksiPengisian14(Kendaraan14 kendaraan, BBM14 bbm, double liter, double totalBayar) {
+        this.kendaraan = kendaraan;
+        this.bbm = bbm;
+        this.liter = liter;
+        this.totalBayar = totalBayar;
+    }
+}
