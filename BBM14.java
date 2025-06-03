4bccd4ab892d6e28d5a3beae717aa59e9544f4c1
arxsyy <marsyfernanda@gmail.com>
2025-06-03 13:56:55 +0700

    update BBM

--- BBM14.java
+++ BBM14.java
@@ -1,13 +1,30 @@
 public class BBM14 {
-    String namaBBM;
+    String namBBM;
     double hargaPerLiter;
 
-    public BBM14 (String namaBBM, double hargaPerLiter) {
-        this.namaBBM = namaBBM;
+    public BBM14(String namBBM, double hargaPerLiter) {
+        this.namBBM = namBBM;
         this.hargaPerLiter = hargaPerLiter;
     }
 
-    void tampilInfo() {
-        System.out.printf("%-20s %-20f");
+    public static BBM14[] listBBM = {
+        new BBM14("Pertalite", 10000),
+        new BBM14("Pertamax", 12400),
+        new BBM14("Biosolar", 6800),
+        new BBM14("DexLite", 13400)
+    };
+
+    public static void tampilBBM() {
+        System.out.printf("%-20s %-20s%n", "Jenis BBM", "Harga"); 
+        for (BBM14 b : listBBM) {
+            System.out.printf("%-20s %-20.0f%n", b.namBBM, b.hargaPerLiter);
+        }
+    }
+
+    public static BBM14 cariBBM(String namBBM) {
+        for (BBM14 b : listBBM) {
+            if (b.namBBM.equalsIgnoreCase(namBBM)) return b;
+        }
+        return null;
     }
 }
